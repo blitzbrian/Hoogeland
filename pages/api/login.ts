84bd985506d8841127d8371464aa20b753958d7c
Dazerstudio <brianvoorbij@gmail.com>
2024-01-31 19:01:49 +0100

    Update Authcode

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -112,7 +112,7 @@ export async function login<Input>(username, password) {
 
     // Hardcoded value in the api, it changes sometimes and then the api rejects all requests
 
-    const authCode = "d2a666be34f1c592";
+    const authCode = "ae2535";
 
     res = await fetch(
         "https://accounts.magister.net" + res.headers.get("location"),
